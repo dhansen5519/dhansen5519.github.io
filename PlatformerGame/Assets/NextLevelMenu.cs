c0aaf3df50db10e0d37421037c1551230079afbf
Darby <daha2377@colorado.edu>
2024-12-13 19:15:45 -0700

    Updated

--- PlatformerGame/Assets/NextLevelMenu.cs
+++ PlatformerGame/Assets/NextLevelMenu.cs
@@ -10,12 +10,14 @@ public class NextLevelMenu : MonoBehaviour
     {
         if (GameOverMenu.lastLevelName == "Level1") {
             SceneManager.LoadScene("Level2");
+            Debug.Log("Level 1 complete");
         }
         else if (GameOverMenu.lastLevelName == "Level2") {
             SceneManager.LoadScene("Level3");
+            Debug.Log("Level 2 complete");
         }
         else {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("AllLevelsCompleteScreen");
             Debug.Log("Last level complete");
         }
     }
@@ -24,5 +26,6 @@ public class NextLevelMenu : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("Return to main menu screen");
     }
 }
